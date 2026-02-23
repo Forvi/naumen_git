2da83f594bf49dcb2c349fbd115b3428856aaaa0
Forvi <nforvi@mail.ru>
2026-02-23 18:41:13 +0500

    Edit file main.java

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ import static java.lang.System.out;
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
     }
 }
